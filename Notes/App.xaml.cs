9cd067f91e1303f13b44a1d284d906b1431b9811
cooperlkeenan <cooperlkeenan@gmail.com>
2025-02-08 15:35:42 +0000

    setting up multiple notes - compiles with no errors  but add button doesn't work

--- Notes/App.xaml.cs
+++ Notes/App.xaml.cs
@@ -5,10 +5,12 @@ namespace Notes;
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
-
-		MainPage = new AppShell();
-	}
+    public App()
+    {
+        InitializeComponent();
+        // Set the MainPage to an instance of AppShell so the Shell navigation appears.
+        MainPage = new AppShell();
+    }
 }
+
+
--- Notes/Views/NotePage.xaml.cs
+++ Notes/Views/NotePage.xaml.cs
@@ -1,31 +1,64 @@
 namespace Notes.Views;
 
+
+[QueryProperty(nameof(ItemId), nameof(ItemId))]
 public partial class NotePage : ContentPage
 {
-    string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
-
+   
+    // Class Constructor
     public NotePage()
     {
         InitializeComponent();
 
-        if (File.Exists(_fileName))
-            TextEditor.Text = File.ReadAllText(_fileName);
+        string appDataPath = FileSystem.AppDataDirectory;
+        string randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
+
+        LoadNote(Path.Combine(appDataPath, randomFileName));
     }
 
 
-    private void SaveButton_Clicked(object sender, EventArgs e)
+
+    // Load Note
+    private void LoadNote(string fileName)
     {
-        // Save the file.
-        File.WriteAllText(_fileName, TextEditor.Text);
+        Models.Note noteModel = new Models.Note();
+        noteModel.Filename = fileName;
+
+        if (File.Exists(fileName))
+        {
+            noteModel.Date = File.GetCreationTime(fileName);
+            noteModel.Text = File.ReadAllText(fileName);
+        }
+
+        BindingContext = noteModel;
     }
 
 
-    private void DeleteButton_Clicked(object sender, EventArgs e)
+    //Save Button
+    private async void SaveButton_Clicked(object sender, EventArgs e)
     {
-        // Delete the file.
-        if (File.Exists(_fileName))
-            File.Delete(_fileName);
+        if (BindingContext is Models.Note note)
+            File.WriteAllText(note.Filename, TextEditor.Text);
 
-        TextEditor.Text = string.Empty;
+        await Shell.Current.GoToAsync("..");
+    }
+
+    //Delete Button
+    private async void DeleteButton_Clicked(object sender, EventArgs e)
+    {
+        if (BindingContext is Models.Note note)
+        {
+            // Delete the file.
+            if (File.Exists(note.Filename))
+                File.Delete(note.Filename);
+        }
+
+        await Shell.Current.GoToAsync("..");
+    }
+
+    //Gets file name of note 
+    public string ItemId
+    {
+        set { LoadNote(value); }
     }
 }
